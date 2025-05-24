dec2f470d993ebec7625c90a4c110706ed5a7478
Chinmayi_AM <chinmayiamc@gmail.com>
2025-05-25 00:19:16 +0530

    Created structure.h

---
+++ structure.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct Employee
+{
+    char Name[100];
+    char ID[10];
+    long int Salary;
+    int Leaves;
+    int Paid_Leaves;
+    long int Claims;
+    int Years;
+}stv_e;
